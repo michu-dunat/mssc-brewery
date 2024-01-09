bebc7c1e08cf82ebfa4c7674d0c89ca3e4c88e1e
Michał Dunat <kontaktmichaldunat@gmail.com>
2024-01-09 13:05:02 +0100

    adjust pom and tests so it can run on my laptop

--- src/test/java/guru/springframework/msscbrewery/MsscBreweryApplicationTests.java
+++ src/test/java/guru/springframework/msscbrewery/MsscBreweryApplicationTests.java
@@ -1,16 +1,15 @@
 package guru.springframework.msscbrewery;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class MsscBreweryApplicationTests {
+public class MsscBreweryApplicationTests
+{
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    void contextLoads()
+    {
+    }
 
 }
